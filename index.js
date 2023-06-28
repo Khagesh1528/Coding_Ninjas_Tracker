01ffb14de4e81e6c1eab7bc32ebec84433c18507
Khagesh1528 <104688846+Khagesh1528@users.noreply.github.com>
2023-06-28 11:28:39 +0530

    new changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 // Connection To MongoDb
 const db = require('./config/mongoose');
@@ -23,9 +23,9 @@ app.set('views','views');
 
 app.use('/', require('./routes'));
 
-app.listen(port,function(err){
+app.listen(PORT,function(err){
     if (err) {
         console.log(`Error in running the server: ${err}`);
     }
-        console.log(`Server is running on port: ${port}`);
+    console.log(`Server is running on port: ${PORT}`);
 });
