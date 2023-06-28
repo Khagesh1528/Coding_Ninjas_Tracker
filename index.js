b5c363aa6067bfab8b6946e9e207bcc6a92e841b
Khagesh1528 <104688846+Khagesh1528@users.noreply.github.com>
2023-06-28 11:32:04 +0530

    install dot env

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8000;
 const app = express();
 // Connection To MongoDb
 const db = require('./config/mongoose');
